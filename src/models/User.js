e46b28a212b839e5c570bc8ec72af03cf9ec0c98
Diego Valenzuela <valenv.dg@gmail.com>
2019-07-21 23:17:28 -0600

    Project initialized. Basic information and packages added.

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(name, email, subscriptions) {
+        this.name = name;
+        this.email = email;
+        this.subscriptions = subscriptions;
+    }
+}
+
+module.exports = User;
---
+++ src/postgreDB.js
@@ -0,0 +1,21 @@
+import { Client } from 'pg';
+
+const client = new Client({
+    user: 'u283hif8hmnds9utdtcz',
+    host: 'b6ko81pxbzztt3wlmlk4-postgresql.services.clever-cloud.com',
+    database: 'b6ko81pxbzztt3wlmlk4',
+    password: 'IOqYJ56B3Lsn151QAQJU',
+    port: '4352'
+});
+
+client.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    else {
+        console.log('Connection Successful.');
+    }
+});
+
+module.exports = client;
